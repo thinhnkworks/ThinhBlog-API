12f9c75cdf93feeb06728a0b623d4d4ebc3be53d
Thinh Nguyen Khang <thinhnk.works@gmail.com>
2023-06-23 13:34:39 +0700

    Test CI/CD

---
+++ Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThinhBlogAPI.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PostController : ControllerBase
+	{
+		[HttpGet(Name = "GetPosts")]
+		public string Get()
+		{
+			return "these are so many posts";
+		}
+	}
+}
